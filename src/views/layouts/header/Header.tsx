8150f4868516b62a45daf206290f0a4daca60b66
Vahe <vpashayan39@gmail.com>
2023-01-22 21:12:25 +0400

    back to latest button

--- src/views/layouts/header/Header.tsx
+++ src/views/layouts/header/Header.tsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { HeaderStyled } from "./HeaderStyled";
 
 export default function Header() {
+  const { pathname } = useLocation();
+
   return (
     <HeaderStyled>
       <Link to={"/"}>
         <h1>news application</h1>
       </Link>
+      {pathname.includes("news") && (
+        <Link to={"/"}>
+          <h1>back to latest news</h1>
+        </Link>
+      )}
     </HeaderStyled>
   );
 }
